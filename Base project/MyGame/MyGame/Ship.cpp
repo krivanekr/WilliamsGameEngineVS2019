d9f6619743498b9c4166b497f17580cfcfae979e
krivanekr <72466940+krivanekr@users.noreply.github.com>
2022-04-29 09:07:17 -0700

    class 4/29

--- Base project/MyGame/MyGame/Ship.cpp
+++ Base project/MyGame/MyGame/Ship.cpp
@@ -1,6 +1,10 @@
 #include "Ship.h"
 
+#include <memory>
+#include "Laser.h"
+
 const float  SPEED = 1.5f;
+const int FIRE_DELAY = 100;
 
 Ship::Ship()
 {
@@ -31,4 +35,21 @@ void Ship::update(sf::Time& elapsed)
 		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Space) && sf::Keyboard::isKeyPressed(sf::Keyboard::D) && pos.x < 805)	x += SPEED * msElapsed * 2;
 
 		sprite_.setPosition(sf::Vector2f(x, y));
+
+		if (fireTimer_ > 0)
+		{
+			fireTimer_ -= msElapsed;
+		}
+
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Enter) && fireTimer_ <= 0)
+		{
+			fireTimer_ = FIRE_DELAY;
+			sf::FloatRect bounds = sprite_.getGlobalBounds();
+
+			float laserX = x + bounds.width;
+			float laserY = y + (bounds.height / 2.0f);
+
+			LaserPtr laser = std::make_shared<Laser>(sf::Vector2f(laserX, laserY));
+			GAME.getCurrentScene().addGameObject(laser);
+		}
 	}
--- Base project/MyGame/MyGame/Ship.h
+++ Base project/MyGame/MyGame/Ship.h
@@ -13,6 +13,7 @@ public:
 	void update(sf::Time& elapsed);
 private:
 	sf::Sprite sprite_;
+	int fireTimer_ = 0;
 };
 
 typedef std::shared_ptr<Ship> ShipPtr;
